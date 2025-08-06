2cd151cfd9dbd3423d0a271e2b0799b9a9bf009f
Red Chien <redchien@viridiasolutions.com>
2025-08-06 15:06:51 +0800

    feat: Question component.

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,26 +1,45 @@
 import { useState, useCallback } from "react";
 import QUESTIONS from "../questions";
 import quizCompleteImg from "../assets/quiz-complete.png";
-import QuestionTimer from "./QuestionTimer.jsx";
+import Question from "./Question.jsx";
 
 export default function Quiz() {
+  const [answerState, setAnswerState] = useState("");
   const [userAnswers, setUserAnswers] = useState([]);
-  console.log("userAnswers :>> ", userAnswers);
+  //   console.log("userAnswers :>> ", userAnswers);
 
   // activeQuestionIndex is the index of the currently active question
   // It is related to the userAnswers array
   // If userAnswers has 0 elements, activeQuestionIndex will be 0,
   // meaning the first question is active
   // So we could set this variable as a computed value
-  const activeQuestionIndex = userAnswers.length;
+  const activeQuestionIndex =
+    answerState === "" ? userAnswers.length : userAnswers.length - 1;
   //   console.log("activeQuestionIndex :>> ", activeQuestionIndex);
 
   const quizIsComplete = activeQuestionIndex >= QUESTIONS.length;
   //   console.log("quizIsComplete :>> ", quizIsComplete);
 
-  const handleSelectAnswer = useCallback((selectedAnswer) => {
-    setUserAnswers((prevAnswers) => [...prevAnswers, selectedAnswer]);
-  }, []);
+  const handleSelectAnswer = useCallback(
+    (selectedAnswer) => {
+      setAnswerState("answered");
+      setUserAnswers((prevAnswers) => [...prevAnswers, selectedAnswer]);
+
+      setTimeout(() => {
+        // In QUESTIONS, the first answer is the correct one
+        if (selectedAnswer === QUESTIONS[activeQuestionIndex].answers[0]) {
+          setAnswerState("correct");
+        } else {
+          setAnswerState("wrong");
+        }
+
+        setTimeout(() => {
+          setAnswerState("");
+        }, 2000);
+      }, 1000);
+    },
+    [activeQuestionIndex]
+  );
 
   const handleSkipAnswer = useCallback(
     () => handleSelectAnswer(null),
@@ -36,33 +55,17 @@ export default function Quiz() {
     );
   }
 
-  // computed value to shuffle the answers for the current question
-  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers].sort(
-    () => Math.random() - 0.5
-  );
-  //   console.log("shuffledAnswers :>> ", shuffledAnswers);
-
   return (
     <div id="quiz">
-      <div id="question">
-        <QuestionTimer
-          key={activeQuestionIndex} // key to reset timer when switching to a new question
-          timeout={5000}
-          onTimeout={handleSkipAnswer}
-        />
-        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
-        <ul id="answers">
-          {shuffledAnswers.map((answer) => {
-            return (
-              <li key={answer} className="answer">
-                <button onClick={() => handleSelectAnswer(answer)}>
-                  {answer}
-                </button>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
+      <Question
+        key={activeQuestionIndex} // set key to re-render Question component when activeQuestionIndex changes
+        questionText={QUESTIONS[activeQuestionIndex].text}
+        answers={QUESTIONS[activeQuestionIndex].answers}
+        onSelectAnswer={handleSelectAnswer}
+        selectedAnswer={userAnswers[userAnswers.length - 1]}
+        answerState={answerState}
+        onSkipAnswer={handleSkipAnswer}
+      />
     </div>
   );
 }
